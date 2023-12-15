6a4a6421a992b5dc500e149885a4c773b1a50303
Wesle Dev <wesle.top@gmail.com>
2023-12-14 22:44:26 -0300

    ajuste deploys

--- src/test/java/com/example/senhas/SenhasApplicationTests.java
+++ src/test/java/com/example/senhas/SenhasApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class SenhasApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
